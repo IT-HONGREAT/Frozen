ff2cd27152194a95ace31ac8f681895d9a746d4d
honginyeong <hongreat95@gmail.com>
2021-07-20 16:18:16 +0900

    7/20 GIS class commit_4; decorators

---
+++ decorators.py
@@ -0,0 +1,16 @@
+
+#인자로 함수를 받는다.
+def decorator(func):
+    def decorated():
+        print('함수시작!')
+        func()
+        print('함수끝!')
+
+    return decorated
+
+@decorator
+def hello_world():
+
+    print('Hello World!')
+
+hello_world()
