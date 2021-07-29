1964f806d50361bd14771bdb132159537e7f68af
honginyeong <hongreat95@gmail.com>
2021-07-29 10:39:06 +0900

    7/29 GIS class commit_4; articleapp 생성 및 초기 적용

--- djangoProject_1/urls.py
+++ djangoProject_1/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
