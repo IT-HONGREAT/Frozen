ea4013915b381c86ae66b76af6dcba0b13985b26
honginyeong <hongreat95@gmail.com>
2021-08-17 15:01:29 +0900

    08/17 GIS class commit_2; create.html 작성

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from projectapp.models import Project
 
 
-class ProjectCrationForm(ModelForm):
+class ProjectCreationForm(ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'image', 'description']
--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 
+from projectapp.views import ProjectCreateView
+
+app_name = 'projectapp'
 
 urlpatterns = [
+    path('create/',ProjectCreateView.as_view(), name='create'),
 
 
 ]
