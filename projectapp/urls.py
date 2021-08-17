f46cb8bc0524cb2ad6971e58bf9ed4c328000cd2
honginyeong <hongreat95@gmail.com>
2021-08-17 15:24:21 +0900

    08/17 GIS class commit_3; projectapp 의 detail 생성

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp'
 
 urlpatterns = [
     path('create/',ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>',ProjectDetailView.as_view(), name='detail')
 
 
 ]
