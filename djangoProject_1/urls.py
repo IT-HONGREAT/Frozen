ed8e44327bde2a62f2809ab8efbd02043e50e56f
honginyeong <hongreat95@gmail.com>
2021-08-19 10:40:37 +0900

    08/19 GIS class commit_5: subscribeapp 생성

--- djangoProject_1/urls.py
+++ djangoProject_1/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accountapp.urls')),
@@ -25,6 +27,7 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
