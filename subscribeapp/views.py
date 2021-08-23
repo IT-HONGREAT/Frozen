abb5e3abc3d27f71e26a8d6b90b0b154e9c53e6e
honginyeong <hongreat95@gmail.com>
2021-08-23 11:07:20 +0900

    08/23 GIS class commit_4: subscribeapp의 list.html을 추가,에러:구독을 눌러도 list에 반영되지않음

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 class SubscriptionView(RedirectView):
@@ -15,7 +17,6 @@ class SubscriptionView(RedirectView):
     def get(self, request, *args, **kwargs):
         user = request.user
         project = Project.objects.get(pk=kwargs['project_pk'])
-
 
         subscription = Subscription.objects.filter(user=user,
                                                    project=project)
@@ -29,3 +30,16 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+
+    paginate_by = 20
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
